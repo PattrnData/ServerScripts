913cc610744446e2d6157bafdce77cb926be5a37
PattrnData <hey@pattrndata.io>
2025-01-05 20:38:56 +0000

    Update stop.sh

--- includes/stop.sh
+++ includes/stop.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-cd /home/steam/zomboid
-sudo -u steam /usr/bin/screen -x zomboid -X stuff 'servermsg "Server is stopping"\n'
-sudo -u steam /usr/bin/screen -x zomboid -X stuff "save\n"
+cd /home/hl-zomboid/zomboid
+sudo -u hl-zomboid /usr/bin/screen -x zomboid -X stuff 'servermsg "Server is stopping"\n'
+sudo -u hl-zomboid /usr/bin/screen -x zomboid -X stuff "save\n"
 sleep 5
-sudo -u steam /usr/bin/screen -x zomboid -X stuff "quit\n"
+sudo -u hl-zomboid /usr/bin/screen -x zomboid -X stuff "quit\n"
 sleep 10
