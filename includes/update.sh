1ec5b08d2d45635d30cc9a55578f60d3dee8f594
PattrnData <hey@pattrndata.io>
2025-01-05 20:40:12 +0000

    Update update.sh

--- includes/update.sh
+++ includes/update.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-/usr/games/steamcmd +force_install_dir /home/steam/zomboid +login anonymous +app_update 380870 validate +quit
+/usr/games/steamcmd +force_install_dir /home/hl-zomboid/zomboid +login anonymous +app_update 380870 validate +quit
